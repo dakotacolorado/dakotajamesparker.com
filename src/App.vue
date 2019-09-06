cab0ca119ae1f512f3a5f78825b988da55091380
Dakota <dakotacolorado2@gmail.com>
2019-09-06 11:07:13 -0600

    -

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <v-app>
     <v-app-bar dense app dark height=60>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Dakota </span>
+      <v-toolbar-title class="headline">
+        <span>Dakota Parker</span>
+        <v-divider vertical></v-divider>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-divider vertical></v-divider>
-      <v-btn
-        text
-        href="https://www.linkedin.com/in/dakota-parker-7a1041137/"
-        target="_blank"
-        style="margin-left:5px"
-      >
-        <img src="@/assets/linkedin.svg" style="margin-left:5px;height:25px"/>
-      </v-btn>
+      <v-toolbar-items>
+        <v-btn
+          text
+          href="https://www.linkedin.com/in/dakota-parker-7a1041137/"
+          target="_blank"
+          style="margin-left:5px"
+        >
+          <img src="@/assets/linkedin.svg" style="margin-left:5px;height:25px"/>
+        </v-btn>
+      </v-toolbar-items>
     </v-app-bar>
 
     <v-content style="height:calc(100vh - 60px)">
@@ -29,7 +31,6 @@ export default {
   components: {
   },
   data: () => ({
-    //
   }),
 };
 </script>
